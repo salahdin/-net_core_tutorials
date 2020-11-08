944841114066b41608a831f00447a48441325b01
salahdin <salahdinjemal@gmail.com>
2020-11-08 08:06:18 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _net_core_tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey whats your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name},on {date:d} at{date:t}!");
+            Console.WriteLine("\n Press any key to exit .....");
+            Console.ReadKey(true);
+
+        }
+    }
+}
